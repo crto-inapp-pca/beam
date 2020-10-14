53b4c7de243a1042e7ed7a9fb16945d88c42ea1f
Vincent Guerci <v.guerci@criteo.com>
2020-10-14 16:00:25 +0200

    Add text scraper

---
+++ Beam/Criteo/CriteoTextScraper.swift
@@ -0,0 +1,24 @@
+//
+//  CriteoTextScraper.swift
+//  beam
+//
+//  Created by Vincent Guerci on 14/10/2020.
+//  Copyright © 2020 Awkward. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func scrapeTextRecursively() -> String? {
+        var texts = [String]()
+        if !accessibilityTraits.contains(.adjustable),
+           let trimmedLabel = accessibilityLabel?.trimmingCharacters(in: .whitespacesAndNewlines),
+           !trimmedLabel.isEmpty {
+            texts.append(trimmedLabel)
+        }
+        if !subviews.isEmpty {
+            texts.append(contentsOf: subviews.reversed().compactMap({ $0.scrapeTextRecursively() }))
+        }
+        return texts.isEmpty ? nil : texts.joined(separator: "\n")
+    }
+}
